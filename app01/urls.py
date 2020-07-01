dffe7fdfc852ff4c057fbaf5ba3c9353176721ef
ruochenPray <1441865605@qq.com>
2020-07-02 01:26:41 +0800

    URL准备

---
+++ app01/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+'''=================================================
+@Project -> File   ：MyDjango -> urls
+@IDE    ：PyCharm
+@Author ：ruochen
+@Date   ：2020/7/2 1:17
+@Desc   ：
+=================================================='''
+from django.conf.urls import url
+from app01 import views
+
+urlpatterns = [
+    url(r'^send/sms/', views.send_sms),
+    url(r'^register/', views.register, name='register'), # "app01:register"
+]
---
+++ web/urls.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+'''=================================================
+@Project -> File   ：MyDjango -> urls
+@IDE    ：PyCharm
+@Author ：ruochen
+@Date   ：2020/7/2 1:18
+@Desc   ：
+=================================================='''
+from django.conf.urls import url
+from web.views import account
+
+urlpatterns = [
+    url(r'^register/$', account.register, name='register'), # register
+]
