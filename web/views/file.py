0a1a48c6be268e453f366741160905e55868348a
ruochenPray <1441865605@qq.com>
2020-07-23 13:36:12 +0800

    文件管理-编辑文件夹

--- web/views/file.py
+++ web/views/file.py
@@ -46,8 +46,18 @@ def file(request, project_id):
         }
         return render(request, 'file.html', context)
 
-    # POST 添加文件夹
-    form = FolderModelForm(request, parent_object, data=request.POST)
+    # POST 添加文件夹 & 文件夹的修改
+    fid = request.POST.get('fid', '')
+    edit_object = None
+    if fid.isdecimal():
+        # 修改
+        edit_object = models.FileRepository.objects.filter(id=int(fid), file_type=2,
+                                                           project=request.tracer.project).first()
+    if edit_object:
+        form = FolderModelForm(request, parent_object, data=request.POST, instance=edit_object)
+    else:
+        form = FolderModelForm(request, parent_object, data=request.POST)
+
     if form.is_valid():
         form.instance.project = request.tracer.project
         form.instance.file_type = 2
